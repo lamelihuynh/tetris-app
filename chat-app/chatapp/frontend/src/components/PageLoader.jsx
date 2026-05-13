6623c1250ed06f9393620b7593c9832f9babc81a
lamelihuynh <gianglinh271@gmail.com>
2026-05-13 11:13:19 +0700

    a

--- chat-app/chatapp/frontend/src/components/PageLoader.jsx
+++
@@ -1,9 +0,0 @@
-import { LoaderIcon } from "lucide-react";
-function PageLoader() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <LoaderIcon className="size-10 animate-spin text-amber-500" />
-    </div>
-  );
-}
-export default PageLoader;
