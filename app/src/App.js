477317c51011395db34285efb60ea803e5027080
lamelihuynh <gianglinh271@gmail.com>
2026-05-20 19:19:27 +0700

    15

---
+++ app/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+import Tetris from "./components/Tetris";
+
+export default function App() {
+  return <Tetris />;
+}
+// new feature
+// new feature
